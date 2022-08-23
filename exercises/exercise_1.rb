5909ed3c2d2a8b7993ddd5ec528cc4da1d60a683
Gloria <gloriacheung812@gmail.com>
2022-08-23 23:47:59 +0000

    remove comment

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,7 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-# Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
 
 store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
