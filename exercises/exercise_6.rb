f86341763c488bd1f24eed27d980e8ecb20d6e40
Gloria <gloriacheung812@gmail.com>
2022-08-25 14:09:41 +0000

    add employees to stores and link via store_id

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Gloria", last_name: "Cheung", hourly_rate: 50)
+@store2.employees.create(first_name: "Andrew", last_name: "Lu", hourly_rate: 55)
+@store2.employees.create(first_name: "Lisa", last_name: "Sun", hourly_rate: 70)
