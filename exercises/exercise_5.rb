d4c4da745cbc7bf5a033ce1c225022feb077a638
Gloria <gloriacheung812@gmail.com>
2022-08-25 13:53:30 +0000

    retrieve sum and avg for annual rev for stores using sum, avg, count, where

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Sum of annual revenue for stores:"
+puts Store.sum(:annual_revenue)
+
+puts "Avg annual revenue for stores:"
+puts Store.average(:annual_revenue)
+
+puts "Number of stores that have over $1M in sales:"
+puts Store.where(annual_revenue: 1000000..).count
